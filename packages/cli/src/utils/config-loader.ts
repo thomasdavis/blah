a3dce777a8a9db2d2dd9b38d5e3ff685a3c4950d
Thomas Davis <thomasalwyndavis@gmail.com>
2025-03-21 18:53:30 +1100

    wip

--- packages/cli/src/utils/config-loader.ts
+++ packages/cli/src/utils/config-loader.ts
@@ -168,6 +168,7 @@ export async function getTools(config: string | Record<string, any>): Promise<an
           await server.connect(transport);
           console.error("JsonResume MCP Server running on stdio");
         }
+          cline does not like additional properties
       */
 
       let listToolsCommandTorun = `echo '{"jsonrpc": "2.0", "method": "tools/list", "params": {}, "id": ${index}}' | ${envString} ${tool.command} -- --config ${config}`;
@@ -192,7 +193,7 @@ export async function getTools(config: string | Record<string, any>): Promise<an
       mcpTools.forEach((mcpTool: McpTool, index) => {
         fullTools.push({
           name: `${tool.name.toUpperCase()}-${index}_${mcpTool.name}`,
-          command: mcpTool.command,
+          // command: mcpTool.command, @todo - if this is the line, write a rant about it somewhere
           description: mcpTool.description,
           inputSchema: mcpTool.inputSchema
         })
